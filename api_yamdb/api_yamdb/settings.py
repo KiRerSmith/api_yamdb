ded97be04bc82b4ef9a16d734c7f1d57738a546f
Vladimir Smirnov <squisheelive@gmail.com>
2021-10-08 14:45:17 +0300

    testing

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -23,6 +23,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework_simplejwt',
+    'reviews',
 ]
 
 MIDDLEWARE = [
@@ -66,7 +67,7 @@ DATABASES = {
     }
 }
 
-
+AUTH_USER_MODEL = 'reviews.User'
 # Password validation
 
 AUTH_PASSWORD_VALIDATORS = [
