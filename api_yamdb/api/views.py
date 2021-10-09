0b3a095c43340bdd34415431af93d139880c2245
Кирилл <kirersmith@gmail.com>
2021-10-09 13:01:18 +0500

    first ver. genre-category-title

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,8 +1,9 @@
+from django_filters.rest_framework import DjangoFilterBackend
 from reviews.models import Category, Genre, Title
 from rest_framework import filters, mixins, permissions, viewsets
 from rest_framework.pagination import LimitOffsetPagination
 
-from .permissions import IsOwnerOrReadOnly, IsModerator
+from .permissions import IsAdmin, AdminOrUser
 from .serializers import CategorySerializer, GenreSerializer, TitleSerializer
 
 
@@ -13,7 +14,8 @@ class CategoryViewSet(mixins.CreateModelMixin,
                       viewsets.GenericViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly, IsModerator,)
+    permission_classes = (AdminOrUser,)
+    pagination_class = LimitOffsetPagination
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name')
 
@@ -25,7 +27,7 @@ class GenreViewSet(mixins.CreateModelMixin,
                    viewsets.GenericViewSet):
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly, IsModerator,)
+    permission_classes = (AdminOrUser,)
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name')
 
@@ -33,5 +35,7 @@ class GenreViewSet(mixins.CreateModelMixin,
 class TitleViewSet(viewsets.ModelViewSet):
     queryset = Title.objects.all()
     serializer_class = TitleSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly, IsModerator,)
+    permission_classes = (AdminOrUser,)
     pagination_class = LimitOffsetPagination
+    filter_backends = (DjangoFilterBackend,)
+    filterset_fields = ('category', 'genre', 'name', 'year')
--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -4,7 +4,7 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 
 class Category(models.Model):
     name = models.CharField(max_length=256, verbose_name='Категория')
-    slug = models.SlugField(max_length=50, unique=True, default='')
+    slug = models.SlugField(max_length=50, unique=True)
 
     def __str__(self):
         return self.name[:20]
@@ -14,8 +14,8 @@ class Category(models.Model):
 
 
 class Genre(models.Model):
-    name = models.CharField(max_length=100, verbose_name='Жанр')
-    slug = models.SlugField(unique=True, default='')
+    name = models.CharField(max_length=256, verbose_name='Жанр')
+    slug = models.SlugField(max_length=50, unique=True)
 
     def __str__(self):
         return self.name[:20]
@@ -25,15 +25,21 @@ class Genre(models.Model):
 
 
 class Title(models.Model):
-    category = models.ForeignKey(Category,
-                                 on_delete=models.CASCADE,
-                                 related_name='title')
-    name = models.CharField(max_length=200, verbose_name='Произведение')
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL,
+        blank=True, null=True,
+        related_name='title'
+    )
+    name = models.CharField(max_length=256, verbose_name='Произведение')
     year = models.IntegerField(
         validators=[
             MinValueValidator(1200),
             MaxValueValidator(2100)
         ]
+    )
+    description = models.TextField(
+        blank=True, null=True,
+        verbose_name='Описание'
     )
 
     def __str__(self):
@@ -41,3 +47,15 @@ class Title(models.Model):
 
     class Meta:
         ordering = ['pk']
+
+
+class GenreTitle(models.Model):
+    title_id = models.ForeignKey(
+        Title, on_delete=models.CASCADE,
+        related_name='title_id'
+    )
+    genre_id = models.ForeignKey(
+        Genre, on_delete=models.SET_NULL,
+        blank=True, null=True,
+        related_name='genre_id'
+    )
